b66d7d0cbb7001c6b6f82d71f8e26e2be8bed904
Braxten Chenay <braxten509@hotmail.com>
2025-06-16 23:39:51 -0600

    Initial Commit

---
+++ ShoppingAppV2/ShoppingAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppV2App.swift
+//  ShoppingAppV2
+//
+//  Created by Braxten Chenay on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShoppingAppV2Tests/ShoppingAppV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppV2Tests.swift
+//  ShoppingAppV2Tests
+//
+//  Created by Braxten Chenay on 6/16/25.
+//
+
+import Testing
+@testable import ShoppingAppV2
+
+struct ShoppingAppV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
